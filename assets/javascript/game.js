fc18225604b71421c29ed4e908a14c4b1fc8d260
Maddie P <madelineprusak@gmail.com>
2019-03-04 15:44:00 -0500

    add reset for the equations. switch up math random equations and add more stying

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,59 +1,69 @@
 // create the target number or the number you are tryin to add up to.
-var targetNumber = 30;  
-  
-
-$("#number-to-guess").text(targetNumber)
-  
 
 // created a variable for the counter.
-var counter = 0;  
+var userCounter = 0;  
+var wins = 0;
+var losses = 0;
+var numberOptions = [3, 4, 2, 7];
+var imageCrystal = $(".crystal-image")
 
 
-var numberOptions = [3, 4, 10, 11];
-for (var i = 0; i < numberOptions.length; i++) {
+// create randomized target number // 
+var targetNumber = Math.floor(Math.random() * 150);
+    console.log(targetNumber);
+        $("#number-to-guess").text(targetNumber);
 
-var imageCrystal = $("<img>");
-
-// var increment = numberOptions[Math.round(Math.random())];
-
-
-        // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-
+// assigns random number to each image
+        $(".crystal-image1").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image2").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image3").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image4").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        
 
   // Here we created an on-click event that responds to button clicks of the crystal image.
-  $(".crystal-image").on("click", function() {
-
-
-
-
+  $(".crystal").on("click", function() {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
 
 
         // We then add the crystalValue to the user's "counter" which is a global variable.
     // Every click, from every crystal adds to the global counter.
- counter += crystalValue;
+ userCounter += crystalValue;
+ console.log(userCounter);
+
+
 
     // We'll then increment the counter each time by the randomly selected number.
     // counter += increment;
     // alert/log the number totaled after each click.
-   alert("Your new score " + counter + " !");
+  //  alert("Your new score " + userCounter + " !");
 
+  
 
-
-  if (counter === targetNumber) {
-
-    // If the numbers match we'll celebrate the user's win.
-    alert("You win!");
+  if (userCounter === targetNumber) {
+    wins++;
+  
+    console.log(wins)
+    // generate new random #'s
+        $(".crystal-image1").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image2").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image3").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image4").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        
+    // // If the numbers match we'll celebrate the user's win.
+    // alert("You win!");
   }
  // Here we added an "else if" condition. If the user's counter ever exceeds the targetNumber...
- else if (counter >= targetNumber) {
-
-    // Then they are alerted with a loss.
-    alert("You lose!!");
-  }
+    else if (userCounter > targetNumber) {
+        losses++;
+        console.log(losses)
+        $(".crystal-image1").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image2").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image3").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+        $(".crystal-image4").attr("data-crystalvalue", Math.floor(Math.random() * 12) + 6)
+ }
+  
 });
-};
+
+
+
